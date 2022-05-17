15c2a92a475aa7f36539300ba187de7de3ddc0e8
NICOTYAN <fumomoSan@outlook.jp>
2022-05-17 17:00:38 +0900

    アイテムの取得

---
+++ Assets/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //座標の変数定義
+    Vector3 m_pos;
+    [SerializeField] float m_speed = 3.2f;
+    [SerializeField] GameObject up;
+
+    private Rigidbody2D rigidBody;
+    private Vector2 inputAxis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //オブジェクトの現在の座標を入手
+        m_pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            up.SetActive(true);
+        }
+        // 矢印キーの入力情報を取得
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        // 移動する向きを作成する
+        Vector2 direction = new Vector2(h, v).normalized;
+
+        // 移動する向きとスピードを代入 
+        GetComponent<Rigidbody2D>().velocity = direction * m_speed;
+    }
+
+
+}
